74775f8a6ec5c8ea1b71c523b6b57d21051e791c
oyeniyi lateef <oyeabdullateef@123.com>
2024-10-01 00:22:32 -0700

    my blog

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const body = document.querySelector("body");
-const themebutton = document.querySelector(".theme");
-let theme = "light";
-function themeChanger() {
-  if (theme == "light") {
-    body.setAttribute("class", "dark");
-    theme = "dark";
-  } else {
-    body.setAttribute("class", "light");
-    theme = "light";
-  }
-}
-themebutton.addEventListener("click", themeChanger);
+// const body = document.querySelector("body");
+// const themebutton = document.querySelector(".theme");
+// let theme = "light";
+// function themeChanger() {
+//   if (theme == "light") {
+//     body.setAttribute("class", "dark");
+//     theme = "dark";
+//   } else {
+//     body.setAttribute("class", "light");
+//     theme = "light";
+//   }
+// }
+// themebutton.addEventListener("click", themeChanger);
+
+// function toggleSidebar() {
+//   const sidebar = document.getElementById("sidebar");
+//   sidebar.classList.toggle("active"); // Toggle sidebar visibility
+// }
