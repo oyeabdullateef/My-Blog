cda2712532c45bf8cceb79b1d81fdb5a63be69cc
oyeniyi lateef <oyeabdullateef@123.com>
2024-10-06 02:54:05 -0700

    my blog

--- blog.js
+++ blog.js
@@ -1,13 +1,1 @@
-const body = document.querySelector("body");
-const themebutton = document.querySelector(".theme");
-let theme = "light";
-function themeChanger() {
-  if (theme == "light") {
-    body.setAttribute("class", "dark");
-    theme = "dark";
-  } else {
-    body.setAttribute("class", "light");
-    theme = "light";
-  }
-}
-themebutton.addEventListener("click", themeChanger);
+
